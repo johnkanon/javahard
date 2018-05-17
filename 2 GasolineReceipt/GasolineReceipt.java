710962c0a85a4788754f2dc9b523d5713b776c71
Kasper Beerman <kbeerman@icscards.nl>
2018-05-17 11:54:02 +0200

    Exercise 2 and changed folder structure

---
+++ 2 GasolineReceipt/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt{
+  public static void main(String[] args){
+    System.out.println("+------------------------+");
+    System.out.println("|                        |");
+    System.out.println("|      CORNER STORE      |");
+    System.out.println("|                        |");
+    System.out.println("| 17-05-18  11:30AM      |");
+    System.out.println("|                        |");
+    System.out.println("| Liters:        5.87    |");
+    System.out.println("| Price/liter: € 1.593   |");
+    System.out.println("|                        |");
+    System.out.println("| Fuel total:  € 9.35    |");
+    System.out.println("|                        |");
+    System.out.println("+------------------------+");
+  }
+}

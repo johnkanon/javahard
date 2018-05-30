cf7739f87e45021157a4cb9dc93bb5aafffc7fee
Kasper Beerman <kbeerman@icscards.nl>
2018-05-30 18:30:52 +0200

    Added exercise 16

---
+++ 16 ComparingNumbers/ComparingNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ComparingNumbers{
+  public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    double first, second;
+
+    System.out.print( "Give me two numbers. First: " );
+    first = keyboard.nextDouble();
+    System.out.print( "Second: " );
+    second = keyboard.nextDouble();
+
+    if (first < second)
+      System.out.println(first+" is less than "+second);
+    if (first <= second)
+      System.out.println(first+" is less than/equal to "+second);
+    if (first == second)
+      System.out.println(first+" is equal to "+second);
+    if (first >= second)
+      System.out.println(first+" is greater than/equal to "+second);
+    if (first > second)
+      System.out.println(first+" is greater than "+second);
+    if (first != second){
+      System.out.println(first+" is not equal to "+second);
+      System.out.println("hey.");
+    }
+  }
+}
